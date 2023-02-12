0fd3e89ce92022276ba497e5e688d163bdd56b22
muhammadhafijur <hafijur.salah@gmail.com>
2023-02-12 16:14:48 +0600

    :sparkles: feat: Async actions part 2

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -3,42 +3,71 @@ import { defineStore } from "pinia";
 export const useTaskStore = defineStore("taskStore", {
   state: () => ({
     tasks: [],
-    loading: false
+    loading: false,
   }),
   getters: {
     favs() {
-        return this.tasks.filter(t => t.isFav)
+      return this.tasks.filter((t) => t.isFav);
     },
     favCount() {
-        return this.tasks.reduce((p, c) => {
-            return c.isFav ? p + 1 : p
-        }, 0)
+      return this.tasks.reduce((p, c) => {
+        return c.isFav ? p + 1 : p;
+      }, 0);
     },
     totalCount: (state) => {
-        return state.tasks.length
-    }
+      return state.tasks.length;
+    },
   },
   actions: {
     async getTasks() {
-        this.loading = true
+      this.loading = true;
 
-        const res = await fetch('http://localhost:3000/tasks')
-        const data = await res.json()
+      const res = await fetch("http://localhost:3000/tasks");
+      const data = await res.json();
 
-        this.tasks = data
-        this.loading = false
+      this.tasks = data;
+      this.loading = false;
     },
-    addTask(task) {
-        this.tasks.push(task)
+    async addTask(task) {
+      this.tasks.push(task);
+
+      const res = await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: {'Content-Type': 'application/json'}
+      });
+
+      if(res.error) {
+        console.log(res.error);
+      }
     },
-    deleteTask(id) {
-        this.tasks = this.tasks.filter(t => {
-            return t.id !== id
-        })
+    async deleteTask(id) {
+      this.tasks = this.tasks.filter((t) => {
+        return t.id !== id;
+      });
+
+
+      const res = await fetch("http://localhost:3000/tasks/" + id, {
+        method: "DELETE",
+      });
+
+      if(res.error) {
+        console.log(res.error);
+      }
     },
-    toggleFav(id) {
-        const task = this.tasks.find(t => t.id === id)
-        task.isFav = !task.isFav
-    }
-  }
-})
+    async toggleFav(id) {
+      const task = this.tasks.find((t) => t.id === id);
+      task.isFav = !task.isFav;
+
+      const res = await fetch("http://localhost:3000/tasks/" + id, {
+        method: "PATCH",
+        body: JSON.stringify({isFav: task.isFav}),
+        headers: {'Content-Type': 'application/json'}
+      });
+
+      if(res.error) {
+        console.log(res.error);
+      }
+    },
+  },
+});
