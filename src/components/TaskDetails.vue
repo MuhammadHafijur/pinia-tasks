29b826d20c04f0cf0dda74ebfa96ab4a513a6041
muhammadhafijur <hafijur.salah@gmail.com>
2023-02-13 11:35:45 +0600

    Fix package lock and added vuetify (#1)

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="task">
+    <v-card class="task ">
+        <div class="d-flex justify-space-between align-center">
+            <h3 class="pa-4">{{ task.title }}</h3>
+        <div class="icons pa-4">
+      <i class="material-icons" @click="taskStore.deleteTask(task.id)"
+        >delete</i
+      >
+      <i
+        class="material-icons"
+        :class="{ active: task.isFav }"
+        @click="taskStore.toggleFav(task.id)"
+        >favorite</i
+      >
+    </div>
+        </div>
+    </v-card>
+  <!-- <div class="task">
     <h3>{{ task.title }}</h3>
     <div class="icons">
       <i class="material-icons" @click="taskStore.deleteTask(task.id)"
@@ -12,7 +28,7 @@
         >favorite</i
       >
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script setup>
